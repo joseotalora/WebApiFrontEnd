b7c11b4ff53849aad3a7061d1f68dda61f5da04c
joseotalora <otalora9110@hotmail.com>
2018-10-11 18:48:52 -0500

    no message

---
+++ WebApi2/Controllers/UsuarioController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApi2.Models;
+
+namespace WebApi2.Controllers
+{
+    public class UsuarioController : ApiController
+    {
+        private BDModel db = new BDModel();
+
+        // GET api/Usuario
+        public IQueryable<USUARIOS> GetUSUARIOS()
+        {
+            return db.USUARIOS;
+        }
+
+        // GET api/Usuario/5
+        [ResponseType(typeof(USUARIOS))]
+        public IHttpActionResult GetUSUARIOS(long id)
+        {
+            USUARIOS usuarios = db.USUARIOS.Find(id);
+            if (usuarios == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(usuarios);
+        }
+
+        // PUT api/Usuario/5
+        public IHttpActionResult PutUSUARIOS(long id, USUARIOS usuarios)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != usuarios.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(usuarios).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!USUARIOSExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST api/Usuario
+        [ResponseType(typeof(USUARIOS))]
+        public IHttpActionResult PostUSUARIOS(USUARIOS usuarios)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.USUARIOS.Add(usuarios);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = usuarios.ID }, usuarios);
+        }
+
+        // DELETE api/Usuario/5
+        [ResponseType(typeof(USUARIOS))]
+        public IHttpActionResult DeleteUSUARIOS(long id)
+        {
+            USUARIOS usuarios = db.USUARIOS.Find(id);
+            if (usuarios == null)
+            {
+                return NotFound();
+            }
+
+            db.USUARIOS.Remove(usuarios);
+            db.SaveChanges();
+
+            return Ok(usuarios);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool USUARIOSExists(long id)
+        {
+            return db.USUARIOS.Count(e => e.ID == id) > 0;
+        }
+    }
+}
